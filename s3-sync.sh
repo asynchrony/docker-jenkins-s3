ade2259f4680d91182c3318da5a5dbbb4439c278
Bruce Harrison <leeharrison1984@gmail.com>
2017-08-21 12:21:23 -0500

    Remove redunant s3 if check

--- s3-sync.sh
+++ s3-sync.sh
@@ -1,3 +1,1 @@
-if [ -n "$S3_BUCKET" ]; then
-  /usr/local/bin/aws s3 sync /var/jenkins_home s3://$S3_BUCKET --sse AES256 --debug
-fi
+/usr/local/bin/aws s3 sync /var/jenkins_home s3://$S3_BUCKET --sse AES256 --debug
