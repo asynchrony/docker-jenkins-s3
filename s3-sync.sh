9c2a6262f636d2b9abbf16580b2e1b930dfc18ba
Bruce <leeharrison1984@gmail.com>
2018-06-25 10:55:43 -0500

    Update excluded directories

--- s3-sync.sh
+++ s3-sync.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/aws s3 sync /var/jenkins_home s3://$S3_BUCKET --sse AES256 --delete --exclude "*workspace/*" --exclude "*lastSuccessful/*" --exclude "*lastStable/*"
+/usr/bin/aws s3 sync /var/jenkins_home s3://$S3_BUCKET --sse AES256 --delete --exclude "*workspace/*" --exclude "*lastSuccessfulBuild/*" --exclude "*lastStableBuild/*" --exclude "*lastStableBuild/*" --exclude "*lastUnsuccessfulBuild/*"
