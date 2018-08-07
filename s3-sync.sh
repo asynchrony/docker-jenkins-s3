e976f451d300d78c3004ce71f5a4c55ca4ad336b
Bruce <leeharrison1984@gmail.com>
2018-08-07 13:16:43 -0500

    Exclude caches from backup

--- s3-sync.sh
+++ s3-sync.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/aws s3 sync /var/jenkins_home s3://$S3_BUCKET --sse AES256 --delete --exclude "*workspace/*" --exclude "*lastSuccessfulBuild/*" --exclude "*lastStableBuild/*" --exclude "*lastFailedBuild/*" --exclude "*lastUnsuccessfulBuild/*"
+/usr/bin/aws s3 sync /var/jenkins_home s3://$S3_BUCKET --sse AES256 --delete --exclude "*caches/*" --exclude "*workspace/*" --exclude "*lastSuccessfulBuild/*" --exclude "*lastStableBuild/*" --exclude "*lastFailedBuild/*" --exclude "*lastUnsuccessfulBuild/*"
